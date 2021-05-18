3395e4d99ed0254acb265353db4ac155a228ab30
antomard800 <64597441+antomard800-hub@users.noreply.github.com>
2021-05-18 10:59:46 +0200

    cross origin

--- src/main/java/com/elis/footballmanager/controller/TeamController.java
+++ src/main/java/com/elis/footballmanager/controller/TeamController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TeamController {
     @Autowired
     TeamService teamService;
--- src/main/java/com/elis/footballmanager/controller/TenantController.java
+++ src/main/java/com/elis/footballmanager/controller/TenantController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TenantController {
     @Autowired
     TenantService tenantService;
